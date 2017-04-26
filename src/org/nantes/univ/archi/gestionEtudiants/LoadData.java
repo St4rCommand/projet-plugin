33cbe0a94d69b3529afe479ca8f12facd69f9db0
denistpnote <denis.b477@gmail.com>
2017-04-26 17:38:43 +0200

    Ajout de l'interface graphique et gestion en mode plugin (interface, stub)

--- src/org/nantes/univ/archi/gestionEtudiants/LoadData.java
+++
@@ -1,7 +0,0 @@
-package org.nantes.univ.archi.gestionEtudiants;
-
-public interface LoadData {
-	
-	public Promotion getPromotion();
-
-}
---
+++ src/org/nantes/univ/archi/plugins/gestionEtudiants/moteurMiageSims/SimsMiageInterface.java
@@ -0,0 +1,12 @@
+package org.nantes.univ.archi.plugins.gestionEtudiants.moteurMiageSims;
+
+import org.nantes.univ.archi.plugins.gestionEtudiants.stubMiageSims.StubInterface;
+
+/**
+ * Created by denis on 26/04/2017.
+ */
+public interface SimsMiageInterface {
+
+    void init(StubInterface stub);
+
+}
